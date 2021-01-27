43fcfd761b4e5de1745a9e081ca38b0c68b6028d
Anaisdg <dganais@gmail.com>
2021-01-27 09:31:33 -0600

    cleaned up code in main.py so that each route calls functions from write_query_data.py. Also fixed query from being hard coded to using current_user.name

--- project/main.py
+++ project/main.py
@@ -8,7 +8,7 @@ import json
 import pandas as pd
 import plotly
 import numpy as np
-from .write_query_data import query_data as qd
+from .write_query_data import query_data, write_data
 
 
 main = Blueprint('main', __name__)
@@ -20,37 +20,8 @@ def index():
 @main.route('/profile')
 @login_required
 def profile():
-    # rng = pd.date_range('1/1/2011', periods=7500, freq='H')
-    # ts = pd.Series(np.random.randn(len(rng)), index=rng)
-    ts = qd()
-
+    ts = query_data()   
     graphs = [
-        # dict(
-        #     data=[
-        #         dict(
-        #             x=[1, 2, 3],
-        #             y=[10, 20, 30],
-        #             type='scatter'
-        #         ),
-        #     ],
-        #     layout=dict(
-        #         title='first graph'
-        #     )
-        # ),
-
-        # dict(
-        #     data=[
-        #         dict(
-        #             x=[1, 3, 5],
-        #             y=[10, 50, 30],
-        #             type='bar'
-        #         ),
-        #     ],
-        #     layout=dict(
-        #         title='second graph'
-        #     )
-        # ),
-
         dict(
             data=[
                 dict(
@@ -75,42 +46,11 @@ def profile():
 #background process happening without any refreshing
 @main.route('/graph_write_data')
 def graph_write_data():
-    my_token = "8aXPa1s0mNMDXexrRDbG5WKd6flwalEg-PnTHhsM-xcPWuEFApJqo1CMrwfjzWNVB_BMZqAHQvBlRKuFJpnhRg=="
-    my_org = "anais@influxdata.com"
-    bucket = "my-bucket"
-    url = "https://us-west-2-1.aws.cloud2.influxdata.com/"
-    client = InfluxDBClient(url=url, token=my_token, org=my_org, debug=False)
-    write_api = client.write_api()
-    df = pd.DataFrame(np.random.randint(0,100,size=(5, 1)), columns=['value'])
-    # datetime object containing current date and time
-    now = datetime.now()
-    dti = pd.date_range(now, periods=5, freq="min")
-    df = df.set_index(dti)
-    print(df)
-    write_api.write(bucket, record=df, data_frame_measurement_name=current_user.name)
-    print("points written")
+    write_data()
     return ("nothing")
  
 #background process happening without any refreshing
 @main.route('/graph_query_data')
 def graph_query_data():
-    print(current_user)
-    my_token = current_user.token
-    # print(my_token)
-    my_org = "anais@influxdata.com"
-    bucket = "my-bucket"
-    query= '''
-    from(bucket: "my-bucket")
-    |> range(start:-30d, stop: now())
-    |> filter(fn: (r) => r._measurement == "three")
-    |> filter(fn: (r) => r["_field"] == "value")
-    |> tail(n:10)'''
-    # print(query)
-    url = "https://us-west-2-1.aws.cloud2.influxdata.com/"
-    client = InfluxDBClient(url=url, token=my_token, org=my_org, debug=False)
-    df = client.query_api().query_data_frame(org=my_org, query=query)
-    value = df["_value"].to_numpy()
-    index = [datetime.to_pydatetime() for datetime in df["_time"]]
-    ts = pd.Series(value, index=index)    
-    print("graph_query", ts)
+    query_data()
     return ts
--- project/write_query_data.py
+++ project/write_query_data.py
@@ -6,28 +6,22 @@ from flask_login import current_user
 from flask_login import login_user, logout_user, login_required
 
 def query_data():
-    # flask_token = "8aXPa1s0mNMDXexrRDbG5WKd6flwalEg-PnTHhsM-xcPWuEFApJqo1CMrwfjzWNVB_BMZqAHQvBlRKuFJpnhRg=="
-    # flask_orgid = "0437f6d51b579000"
-    # print(current_user)
     my_token = current_user.token
-    # my_token = "8aXPa1s0mNMDXexrRDbG5WKd6flwalEg-PnTHhsM-xcPWuEFApJqo1CMrwfjzWNVB_BMZqAHQvBlRKuFJpnhRg=="
-    print(my_token)
+    my_user = current_user.name
     my_org = "anais@influxdata.com"
     bucket = "my-bucket"
     query= '''
     from(bucket: "my-bucket")
     |> range(start:-30d, stop: now())
-    |> filter(fn: (r) => r._measurement == "three")
+    |> filter(fn: (r) => r._measurement == "''' + my_user + '''")
     |> filter(fn: (r) => r["_field"] == "value")
     |> tail(n:10)'''
-    # print(query)
     url = "https://us-west-2-1.aws.cloud2.influxdata.com/"
     client = InfluxDBClient(url=url, token=my_token, org=my_org, debug=False)
     df = client.query_api().query_data_frame(org=my_org, query=query)
     value = df["_value"].to_numpy()
     index = [datetime.to_pydatetime() for datetime in df["_time"]]
     ts = pd.Series(value, index=index)    
-    # print(ts)
     return ts
 
 def write_data():
@@ -36,13 +30,14 @@ def write_data():
     bucket = "my-bucket"
     url = "https://us-west-2-1.aws.cloud2.influxdata.com/"
     client = InfluxDBClient(url=url, token=my_token, org=my_org, debug=False)
-    df = pd.DataFrame(np.random.randint(0,100,size=(5, 1)), columns=list('value'))
+    write_api = client.write_api()
+    df = pd.DataFrame(np.random.randint(0,100,size=(5, 1)), columns=['value'])
     # datetime object containing current date and time
     now = datetime.now()
-    dti = pd.date_range(now, periods=3, freq="M")
+    dti = pd.date_range(now, periods=5, freq="min")
     df = df.set_index(dti)
-    write_client.write(bucket, record=df, data_frame_measurement_name=current_user.name)
-    
+    print(df)
+    write_api.write(bucket, record=df, data_frame_measurement_name=current_user.name)
     return print("points written")
  
 if __name__ == "__main__":
